decda09211ee6e6f7888f9b8112f562661a01100
sneha nishad <snehanishad9927@gmail.com>
2026-05-31 14:53:35 +0530

    bootstrap

---
+++ RCT1/my-react-app/src/main.jsx
@@ -0,0 +1,17 @@
+import { createRoot } from 'react-dom/client'
+const myelement =(
+  <table>
+  <th>name
+    <td>sneha</td>
+  </th>
+  <th>class
+    <td>cse26</td>
+  </th>
+  <th>roll no 
+    <td>45</td>
+  </th>
+  </table>
+);
+createRoot(document.getElementById('root')).render(
+myelement
+)  
